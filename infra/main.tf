3d4681b03441337e64ee2ab60492cf19589ea64a
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2025-02-08 11:18:14 +0100

    Deploy first GCP Bigquery environment #5

--- infra/main.tf
+++ infra/main.tf
@@ -1,28 +1,4 @@
-#### Create GCP Cloud Storage bucket ####
-
-module "cloud_storage_bucket" {
-  source                    = "../modules/cloud_storage_bucket"
-  project                   = var.project_id
-  zone                      = var.zone
-  bucket                    = var.bucket
-  location                  = var.location
-  versioning_enabled        = var.versioning_enabled
-  lifecycle_rule_age        = var.lifecycle_rule_age
-  bucket_owner_email        = var.bucket_owner_email  
-}
-
-# Fetch input file from GitHub (Corrected URL)
-data "http" "bucket" {
-  url                         = "https://raw.githubusercontent.com/GoogleCloudPlatform/DataflowTemplates/master/apache_beam_wordcount/README.md"
-}
-
-resource "google_storage_bucket_object" "input_file" {
-  bucket                    = module.cloud_storage_bucket.bucket_name
-  name                      = "inputs/readme.md"
-  content                   = data.http.bucket.response_body # Correction: Utilisation de response_body
-}
-
-### BigQuery Deployment ###
+### Activate GCP Project Services APIs ###
 resource "google_project_service" "bigquery_api" {
   project                     = var.project_id
   service                     = "bigquery.googleapis.com"
@@ -30,25 +6,6 @@ resource "google_project_service" "bigquery_api" {
   disable_dependent_services  = true
 }
 
-module "bigquery_dataset" {
-  source                      = "../modules/bigquery"
-  project                     = var.project_id
-  location                    = var.location
-  dataset_id                  = var.dataset_id
-  dataset_description         = var.dataset_description
-  default_table_expiration_ms = var.default_table_expiration_ms
-  dataset_owner_email         = var.dataset_owner_email
-  data_editor_group           = var.data_editor_group
-  data_viewer_group           = var.data_viewer_group
-  depends_on                  = [google_project_service.bigquery_api] # Ensure API is enabled first
-}
-
-output "dataset_self_link" {
-  value       = module.bigquery_dataset.dataset_self_link  # Corrected line
-  description = "The self link of the created dataset"
-}
-
-### Activate GCP Project Services APIs ###
 resource "google_project_service" "dataflow_api" {
   project                     = var.project_id
   service                     = "dataflow.googleapis.com"
@@ -89,6 +46,48 @@ resource "google_project_service" "servicenetworking_api" {
   project = var.project_id
   service            = "servicenetworking.googleapis.com"
   disable_on_destroy = false
+}
+
+#### Create GCP Cloud Storage bucket ####
+module "cloud_storage_bucket" {
+  source                    = "../modules/cloud_storage_bucket"
+  project                   = var.project_id
+  zone                      = var.zone
+  bucket                    = var.bucket
+  location                  = var.location
+  versioning_enabled        = var.versioning_enabled
+  lifecycle_rule_age        = var.lifecycle_rule_age
+  bucket_owner_email        = var.bucket_owner_email  
+}
+
+# Fetch input file from GitHub (Corrected URL)
+data "http" "bucket" {
+  url                         = "https://raw.githubusercontent.com/GoogleCloudPlatform/DataflowTemplates/master/apache_beam_wordcount/README.md"
+}
+
+resource "google_storage_bucket_object" "input_file" {
+  bucket                    = module.cloud_storage_bucket.bucket_name
+  name                      = "inputs/readme.md"
+  content                   = data.http.bucket.response_body # Correction: Utilisation de response_body
+}
+
+### BigQuery Deployment ###
+module "bigquery_dataset" {
+  source                      = "../modules/bigquery"
+  project                     = var.project_id
+  location                    = var.location
+  dataset_id                  = var.dataset_id
+  dataset_description         = var.dataset_description
+  default_table_expiration_ms = var.default_table_expiration_ms
+  dataset_owner_email         = var.dataset_owner_email
+  data_editor_group           = var.data_editor_group
+  data_viewer_group           = var.data_viewer_group
+  depends_on                  = [google_project_service.bigquery_api] # Ensure API is enabled first
+}
+
+output "dataset_self_link" {
+  value       = module.bigquery_dataset.dataset_self_link  # Corrected line
+  description = "The self link of the created dataset"
 }
 
 # Dataflow Job (Corrected)
