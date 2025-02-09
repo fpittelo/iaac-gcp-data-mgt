5da9a8dee00641d6ce4f7f8e310f53a15ce1dd26
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2025-02-09 14:43:55 +0100

    Create 2 Bigquery Tables #27

--- modules/bigquery/outputs.tf
+++ modules/bigquery/outputs.tf
@@ -3,3 +3,8 @@ output "dataset_self_link" {
   value = google_bigquery_dataset.dataset.self_link
   description = "The self link of the created dataset"
 }
+
+output "dataset_id" {
+  value = google_bigquery_dataset.dataset.dataset_id
+  description = "The ID of the created dataset"
+}
