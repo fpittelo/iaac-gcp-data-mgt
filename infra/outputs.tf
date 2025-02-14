b4f50a3b778a180c00b8dba40aea1440335235d1
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2025-02-14 08:03:46 +0100

    Create 2 Bigquery Tables #27

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,3 +8,7 @@ output "member_roles" {
   value = google_project_iam_member.multiple_roles
 }
 
+output "dataset_self_links" {
+  value = { for key, dataset in module.google_bigquery_dataset: key => dataset.dataset_self_link }
+  description = "Self links of the created datasets"
+}
--- modules/bucket/outputs.tf
+++ modules/bucket/outputs.tf
@@ -3,11 +3,6 @@ output "bucket" {
   value = google_storage_bucket.bucket.name
 }
 
-output "dataset_self_links" {
-  value = { for key, dataset in module.google_bigquery_dataset: key => dataset.dataset_self_link }
-  description = "Self links of the created datasets"
-}
-
-/* output "bucket_url" {
+output "bucket_url" {
   value = google_storage_bucket.bucket.self_link
-} */
+}
