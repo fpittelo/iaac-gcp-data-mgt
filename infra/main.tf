f4639f3424ada0d1156022d528063b0310c85436
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2025-02-18 08:41:56 +0100

    create data transfer for academia data #61

--- infra/main.tf
+++ infra/main.tf
@@ -155,7 +155,7 @@ resource "null_resource" "create_opr_swissgrid_csv" {
 
 resource "google_storage_bucket_object" "ac_schools" {
   bucket = "inputs-acd-data"  # Ou une variable si vous préférez
-  name   = "ac_schools.csv"
+  name   = "inputs/ac_schools.csv"
   source = "${path.module}/inputs/ac_schools.csv"
 }
 
