182997a5165f589cc112e9326e85786e7c86bc07
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2025-02-19 16:52:36 +0100

    #1 update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,29 +1,31 @@
 terraform {
   required_providers {
     google = {
-      source    = "hashicorp/google"
-      version   = "=6.20.0"
+      source  = "hashicorp/google"
+      version = "~> 6.20.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.20.0"
     }
     http = {
       source  = "terraform-aws-modules/http"
       version = "~> 2.0"
     }
   }
-  
+
   backend "gcs" {
-    bucket      = "iaac-gcp-data-mgt-terraform"
-    prefix      = "backend/terraform/state"
-  # use_oidc  = true
+    bucket = "iaac-gcp-data-mgt-terraform"
+    prefix = "backend/terraform/state"
   }
 }
 
 provider "google" {
-  project       = var.project_id
-  region        = var.location
-# zone          = var.zone
+  project = var.project_id
+  region  = var.location
 }
 
-/* provider "google-beta" {
-  project       = var.project_id
-  region        = var.location
-} */
+provider "google-beta" {
+  project = var.project_id
+  region  = var.location
+}
