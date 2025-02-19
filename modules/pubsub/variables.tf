be3b9c9efbeb6c24ad4d75ba74b8b3cfc690abe7
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2025-02-19 16:34:36 +0100

    #74

--- modules/pubsub/variables.tf
+++ modules/pubsub/variables.tf
@@ -1,17 +1,31 @@
 ### ModulesVariables ###
-
-variable "project_id" {
-    description = "The GCP project ID"
-    type        = string
+variable "topics" {
+  type = map(object({
+    name                       = string
+    labels                     = map(string)
+    allowed_persistence_regions = list(string)
+    kms_key_name               = string
+    schema                     = string
+    encoding                   = string
+  }))
+  description = "Liste des topics Pub/Sub à créer"
+  default     = {}
 }
 
-variable "topic_name" {
-    description = "The name of the Pub/Sub topic"
-    type        = string
-}
-
-variable "labels" {
-  type = map(string)
-  description = "Labels to apply to resources"
-  default = {}
+variable "subscriptions" {
+  type = map(object({
+    name                       = string
+    topic                      = string
+    labels                     = map(string)
+    ack_deadline_seconds       = number
+    message_retention_duration = string
+    retain_acked_messages      = bool
+    filter                     = string
+    push_endpoint              = string
+    dead_letter_topic          = string
+    max_delivery_attempts      = number
+    ttl                        = string
+  }))
+  description = "Liste des abonnements Pub/Sub à créer"
+  default     = {}
 }
