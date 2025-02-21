8b63e37b05d59760f18b69a5073e469f5be1859a
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2025-02-21 13:35:22 +0100

    #77 dataplex apis activation + disable on destroy true

--- infra/main.tf
+++ infra/main.tf
@@ -35,9 +35,12 @@ resource "google_project_service" "api_activations" {
     "pubsub.googleapis.com",
     "secretmanager.googleapis.com",
     "storage.googleapis.com",
+    "dataplex.googleapis.com", # Dataplex API
+    "datacatalog.googleapis.com", # Data Catalog API (Dataplex dependency)
+    "datalineage.googleapis.com", # Data Lineage API (Optional, but commonly used)
   ])
   service            = each.key
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 resource "google_project_iam_member" "service_account_storage_admin" {
