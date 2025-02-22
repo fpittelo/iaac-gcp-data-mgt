aa4bdb582290beb0fffe1725e133b6b414cf01c0
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2025-02-22 10:50:18 +0100

    Set up dataplex #80

--- modules/dataplex_lake/main.tf
+++ modules/dataplex_lake/main.tf
@@ -1,7 +1,7 @@
 resource "google_dataplex_lake" "default" {
   name              = var.lake_name
   labels            = var.labels
-  location          = var.location
+  location          = "europe-west1"
 
     metastore {
       service         = "projects/${var.project_id}/locations/${var.location}/services/${var.metastore_service_id}"
@@ -11,7 +11,7 @@ resource "google_dataplex_lake" "default" {
 resource "google_dataplex_zone" "raw_zone" {
   name              = "${var.lake_name}-raw"
   lake              = google_dataplex_lake.default.id
-  location          = var.location
+  location          = "europe-west1"
   type              = "RAW"
   description       = "Raw data zone for the lake"
   labels            = var.labels
@@ -26,7 +26,7 @@ resource "google_dataplex_zone" "raw_zone" {
 resource "google_dataplex_zone" "curated_zone" {
   name              = "${var.lake_name}-curated"
   lake              = google_dataplex_lake.default.id
-  location          = var.location
+  location          = "europe-west1"
   type              = "CURATED"
   description       = "Curated data zone for the lake"
   labels            = var.labels
